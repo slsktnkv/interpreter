f89f1d914d08acd1a825040aa43a1e723276bd9d
slsktnkv <slsktnkv@gmail.com>
2026-03-11 01:39:48 +0300

    Fix test interpreter

--- tests/test_interpreter.cpp
+++ tests/test_interpreter.cpp
@@ -24,18 +24,46 @@ TEST_CASE("SetAndAdd", "[Interpreter][Add]") {
 }
 
 /*
-TEST_CASE("LinkedAdd", "[Interpreter][Add][Link]") {
+TEST_CASE("Strings", "[Interpreter][Add][String]") {
     std::string actual;
     std::ostringstream output(actual);
     Interpreter interpreter(output);
-    interpreter.Evaluate("SET a 1");
     interpreter.Evaluate("SET b a");
-    interpreter.Evaluate("ADD a 2");
-    interpreter.Evaluate("PRINT a");
+    interpreter.Evaluate("ADD b a");
     interpreter.Evaluate("PRINT b");
-    interpreter.Evaluate("ADD b 3");
-    interpreter.Evaluate("PRINT a");
-    interpreter.Evaluate("PRINT b");
-    REQUIRE(output.str() == "3\n3\n6\n6\n");
+    REQUIRE(output.str() == "aa\n");
+}
+
+TEST_CASE("StringsMixed", "[Interpreter][Add][String]") {
+    std::string actual;
+    std::ostringstream output(actual);
+    Interpreter interpreter(output);
+    interpreter.Evaluate("SET i 0");
+    interpreter.Evaluate("SET s a");
+    interpreter.Evaluate("ADD i 1");
+    interpreter.Evaluate("ADD s 1");
+    interpreter.Evaluate("PRINT i");
+    interpreter.Evaluate("PRINT s");
+    REQUIRE(output.str() == "1\na1\n");
+}
+
+TEST_CASE("ListsAppend", "[Interpreter][Add][List]") {
+    std::string actual;
+    std::ostringstream output(actual);
+    Interpreter interpreter(output);
+    interpreter.Evaluate("SET l [1 2 3]");
+    interpreter.Evaluate("ADD l 4");
+    interpreter.Evaluate("PRINT l");
+    REQUIRE(output.str() == "[1 2 3 4]\n");
+}
+
+TEST_CASE("ListsConcat", "[Interpreter][Add][List]") {
+    std::string actual;
+    std::ostringstream output(actual);
+    Interpreter interpreter(output);
+    interpreter.Evaluate("SET l [1 2 3]");
+    interpreter.Evaluate("ADD l [4 5 6]");
+    interpreter.Evaluate("PRINT l");
+    REQUIRE(output.str() == "[1 2 3 4 5 6]\n");
 }
 */
