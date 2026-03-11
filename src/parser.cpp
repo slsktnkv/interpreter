8aff545aac31de3aaa5fed2cabbc5eb104f165ac
Stanislav Loskutnikov <sloskutnikov@10.215.128.188-vpn.dhcp.yndx.net>
2026-03-11 11:36:07 +0300

    use MakeValue

--- src/parser.cpp
+++ src/parser.cpp
@@ -16,7 +16,7 @@ Integer MakeInteger(std::string_view operand) {
 }
 
 Value* MakeValue(std::string_view operand) {
-
+    
 }
 
 Command MakeCommand(const std::vector<std::string_view>& tokens) {
@@ -28,11 +28,11 @@ Command MakeCommand(const std::vector<std::string_view>& tokens) {
     if (tokens.size() == 3) {
         if (tokens[0] == "SET") {
             return Command{
-                .type = Command::Type::Set, .var = std::string(tokens[1]), .operand = MakeInteger(tokens[2])};
+                .type = Command::Type::Set, .var = std::string(tokens[1]), .operand = MakeValue(tokens[2])};
         }
         if (tokens[0] == "ADD") {
             return Command{
-                .type = Command::Type::Add, .var = std::string(tokens[1]), .operand = MakeInteger(tokens[2])};
+                .type = Command::Type::Add, .var = std::string(tokens[1]), .operand = MakeValue(tokens[2])};
         }
     }
     throw std::invalid_argument("Invalid syntax");
